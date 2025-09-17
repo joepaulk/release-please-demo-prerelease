487f81e30cfed53583b891d286c1061292f3eaa8
Joe <joepaul.k@gmail.com>
2025-09-17 16:39:15 +1000

    chore(main): release 1.11.0-rc.3

--- custom-version-update-example/build.gradle.kts
+++ custom-version-update-example/build.gradle.kts
@@ -72,7 +72,7 @@ publishing {
                 groupId = "llc.sonderformat"
                 artifactId = "sonderformat-example-sdk"
                 // x-release-please-start-version
-                version = "1.11.0-rc.2"
+                version = "1.11.0-rc.3"
                 // x-release-please-end
 
                 // And here are some more properties that go into the pom file.
